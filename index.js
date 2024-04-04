7f4ab273d8a886e87d4e1d5af7ddf9f39768bc1b
Minchan <minchanhan5@gmail.com>
2024-04-04 13:39:28 -0400

    reactivate server running log

--- index.js
+++ index.js
@@ -395,6 +395,6 @@ io.on("connection", (socket) => {
 });
 
 server.listen(process.env.PORT, () => {
-  // console.log(`Server is running on port ${process.env.PORT}..`);
+  console.log(`Server is running on port ${process.env.PORT}..`);
 });
 
